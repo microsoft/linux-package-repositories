df66e1c259a829cf1c94551ef3f00cc24a7429c2
Andrew Cheng <cheng.andrew@yahoo.com>
2023-05-03 08:50:08 -0700

    Initial commit for new --apt-source option

--- repoaudit/repoaudit/__init__.py
+++ repoaudit/repoaudit/__init__.py
@@ -34,6 +34,38 @@ pubkey_option = click.option(
     )
 )
 
+apt_sources_option = click.option(
+    "--apt-source",
+    "-a",
+    required=False,
+    help=(
+        "Supply apt sources.list file. When provided, all entries"
+        "will be parsed for repo urls and their respective dists."
+    )
+)
+
+def apt_helper(recursive: bool, url: str, dists: str, output: str, pubkeys: str) -> None:
+    """Helps apt function validate an apt repository at url"""
+    if recursive:
+        urls = get_repo_urls(url)
+    else:
+        urls = [url]
+
+    if dists:
+        dist_set = set(dists.split(","))
+    else:
+        dist_set = None
+
+    errors = RepoErrors()
+
+    with _gpg_cmdline(pubkeys) as gpg:
+        try:
+            for repo_url in urls:
+                check_apt_repo(repo_url, dist_set, gpg, errors)
+        except KeyboardInterrupt:
+            pass
+
+    output_result(errors, output)
 
 @contextmanager
 def _gpg_cmdline(pubkeys: str):
@@ -62,30 +94,21 @@ def main() -> None:
 @recursive_option
 @click.argument("url")
 @click.option("--dists", help="Comma separated list of distributions.")
+@apt_sources_option
 @file_option
 @pubkey_option
-def apt(recursive: bool, url: str, dists: str, output: str, pubkeys: str) -> None:
+def apt(recursive: bool, url: str, dists: str, apt_source: str, output: str, pubkeys: str) -> None:
     """Validate an apt repository at URL."""
-    if recursive:
-        urls = get_repo_urls(url)
+    if apt_source:
+        with open(apt_source, "r") as f:
+            lines = [line.strip() for line in f if line.startswith("deb")]
+            for line in lines:
+                fields = line.split(" ")
+                _url = fields[1]
+                _dists = ','.join(fields[slice(2, len(fields))])
+                apt_helper(recursive, _url, _dists, output, pubkeys)
     else:
-        urls = [url]
-
-    if dists:
-        dist_set = set(dists.split(","))
-    else:
-        dist_set = None
-
-    errors = RepoErrors()
-
-    with _gpg_cmdline(pubkeys) as gpg:
-        try:
-            for repo_url in urls:
-                check_apt_repo(repo_url, dist_set, gpg, errors)
-        except KeyboardInterrupt:
-            pass
-
-    output_result(errors, output)
+        apt_helper(recursive, url, dists, output, pubkeys)
 
 
 @main.command()
