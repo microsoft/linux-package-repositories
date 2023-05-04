bd0d36a58f81a289290dbf6dde5d9aa5527796e6
acheng-01 <91927604+acheng-01@users.noreply.github.com>
2023-05-04 14:51:33 -0700

    New Feature: --apt-source option for sources.list files (#55)

--- repoaudit/repoaudit/__init__.py
+++ repoaudit/repoaudit/__init__.py
@@ -35,6 +35,30 @@ pubkey_option = click.option(
 )
 
 
+def _validate_apt_url(recursive: bool, url: str, dists: str, output: str, pubkeys: str) -> None:
+    """Validate an apt repository at url"""
+    if recursive:
+        urls = get_repo_urls(url)
+    else:
+        urls = [url]
+
+    if dists:
+        dist_set = set(dists.split(","))
+    else:
+        dist_set = None
+
+    errors = RepoErrors()
+
+    with _gpg_cmdline(pubkeys) as gpg:
+        try:
+            for repo_url in urls:
+                check_apt_repo(repo_url, dist_set, gpg, errors)
+        except KeyboardInterrupt:
+            pass
+
+    output_result(errors, output)
+
+
 @contextmanager
 def _gpg_cmdline(pubkeys: str):
     gpg = None
@@ -60,32 +84,41 @@ def main() -> None:
 
 @main.command()
 @recursive_option
-@click.argument("url")
-@click.option("--dists", help="Comma separated list of distributions.")
+@click.argument("url", required=False)
+@click.option("--dists", required=False, help="Comma separated list of distributions.")
+@click.option(
+    "--apt-source",
+    "-a",
+    required=False,
+    type=click.File("r"),
+    help=(
+        "Supply apt sources.list file. When provided, all entries"
+        "will be parsed for repo urls and their respective dists."
+    )
+)
 @file_option
 @pubkey_option
-def apt(recursive: bool, url: str, dists: str, output: str, pubkeys: str) -> None:
+def apt(recursive: bool, url: str, dists: str, apt_source: str, output: str, pubkeys: str) -> None:
     """Validate an apt repository at URL."""
-    if recursive:
-        urls = get_repo_urls(url)
-    else:
-        urls = [url]
-
-    if dists:
-        dist_set = set(dists.split(","))
-    else:
-        dist_set = None
-
-    errors = RepoErrors()
-
-    with _gpg_cmdline(pubkeys) as gpg:
+    if apt_source:
         try:
-            for repo_url in urls:
-                check_apt_repo(repo_url, dist_set, gpg, errors)
-        except KeyboardInterrupt:
-            pass
-
-    output_result(errors, output)
+            lines = [line.strip() for line in apt_source if line.startswith("deb")]
+            for line in lines:
+                fields = line.split(" ")
+                index: int
+                if (fields[1].startswith("http")):
+                    index = 1
+                elif (fields[2].startswith("http")):
+                    index = 2
+                else:
+                    raise Exception("No URL found")
+                url = fields[index]
+                dists = fields[(index + 1)]
+                _validate_apt_url(recursive, url, dists, output, pubkeys)
+        except IOError:
+            print("Error: Unable to open file")
+    else:
+        _validate_apt_url(recursive, url, dists, output, pubkeys)
 
 
 @main.command()
