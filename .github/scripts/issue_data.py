711ca68539fcb463bb4a339469a3c418d9b04fba
adrianabedon <56134101+adrianabedon@users.noreply.github.com>
2024-07-01 08:29:06 -0700

    Sync GitHub Issues (#144)
    * issue sync

---
+++ .github/scripts/issue_data.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import datetime
+import json
+
+from github import Github, Auth
+
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN", None)
+if not GITHUB_TOKEN:
+    raise ValueError("GITHUB_TOKEN is not set")
+
+full_name = sys.argv[1]
+number = int(sys.argv[2])
+
+auth = Auth.Token(GITHUB_TOKEN)
+g = Github(auth=auth)
+
+repo = g.get_repo(full_name)
+issue = repo.get_issue(number)
+
+query_date = datetime.datetime.now(datetime.UTC) - datetime.timedelta(hours=1)
+
+unique_comment_authors = {
+    comment.user.login for comment in issue.get_comments(query_date) if comment.user
+}
+
+unique_reaction_authors = {
+    reaction.user.login
+    for reaction in issue.get_reactions()
+    if reaction.user and reaction.created_at > query_date
+}
+
+mentions_count = sum(
+    map(
+        lambda e: e.event in ["cross-referenced", "referenced"]
+        and e.created_at > query_date,
+        issue.get_timeline(),
+    )
+)
+
+data = {
+    "unique_comments": len(unique_comment_authors),
+    "unique_reactions": len(unique_reaction_authors),
+    "mentions": mentions_count,
+}
+
+with open("issue_data.json", "w") as f:
+    json.dump(data, f)
